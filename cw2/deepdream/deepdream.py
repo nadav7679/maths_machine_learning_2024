b582d165b5c39be8c73cca2b7fa617a3bb8a8c48
nadav7679 <nadav76799@gmail.com>
2024-03-27 13:33:58 +0000

    Deepdream class

--- cw2/deepdream/deepdream.py
+++ cw2/deepdream/deepdream.py
@@ -1,81 +1,78 @@
-import io
-import requests
-
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 import torch
-from torchvision import transforms
-from PIL import Image
+import matplotlib.pyplot as plt
 
 
-model: torch.nn.Module = torch.hub.load('pytorch/vision:v0.10.0', 'inception_v3', pretrained=True)
-model.eval()
+class Deepdream:
+    def __init__(self, target_modules, iterations, lr):
+        self.target_modules = target_modules
+        self.iterations = iterations
+        self.lr = lr
+
+        self.model: torch.nn.Module = torch.hub.load('pytorch/vision:v0.10.0', 'inception_v3', pretrained=True)
+        self.model.eval()
+
+        modules = {}
+        for name, module in self.model.named_modules():
+            modules[name] = module
+
+        #: A dictionary with (key,value) = (module name, reference (pointer) to module) for all modules.
+        self.modules = modules
+        #: A dictionary with (key,value) = (module name, module activation output)
+        self.activation = {}
+
+        # Register the target layers in the forward pass
+        for module_name in target_modules:
+            self.modules[module_name].register_forward_hook(self.get_activation(module_name))
+
+    def get_activation(self, module_name):
+        """
+        Using closure (on self.activation) to save the activations outputs of a certain module.
+
+        :param module_name: The name of the module we are targeting.
+        :return: Callable function hook.
+        """
+        def hook(model, input, output):
+            self.activation[module_name] = output
+        return hook
+
+    def deepdream(self, x: torch.Tensor, normalize=False):
+        x = x.unsqueeze(0) if len(x.shape)==3 else x
+        x.requires_grad = True
+
+        loss = torch.nn.MSELoss(reduction="sum")
+        for _ in range(self.iterations):
+            # Forward pass input data
+            self.model(x)
+
+            # Calculate target layers loss
+            tot_loss = torch.tensor(0., requires_grad=True)
+            for _, a in self.activation.items():
+                a_loss = loss(a, torch.zeros_like(a))
+                a_loss.retain_grad()
+                tot_loss = tot_loss + a_loss
+
+            tot_loss.backward()
+
+            g = x.grad
+            if normalize:
+                g -= g.mean()
+                g /= g.std()
+            else:
+                g *= torch.abs(g).mean()
+
+            with torch.no_grad():
+                x += self.lr * g.detach()
+
+            self.model.zero_grad()
+
+        return x
 
 
-def load_image(url, show=False):
-    # TODO: first preprocess, then show image
+    @staticmethod
+    def get_module_names():
+        model = torch.hub.load('pytorch/vision:v0.10.0', 'inception_v3', pretrained=True)
+        return [name for name, _ in model.named_modules()]
 
-    img = requests.get(url).content
-
-    with io.BytesIO(img) as file:
-        img = mpimg.imread(file, format="jpg")
-
-    if show:
-        plt.imshow(img)
-        plt.show()
-
-    preprocess = transforms.Compose([
-        transforms.ToPILImage(),
-        transforms.Resize(299),
-        transforms.CenterCrop(299),
-        transforms.ToTensor(),
-    ])
-    img = np.transpose(img, (2, 0, 1))
-    return preprocess(torch.tensor(img))
-
-
-activation = {}
-def get_activation(name):
-    global activation
-    def hook(model, input, output):
-        activation[name] = output
-    return hook
-
-
-def deepdream_optim(iterations: int, lr: float, x: torch.Tensor, module, module_name, normalize=False):
-    # TODO: make getting the activations less ugly
-    global model
-
-    x = x.unsqueeze(0) if len(x.shape)==3 else x
-    x.requires_grad = True
-    module.register_forward_hook(get_activation(module_name))
-    loss = torch.nn.MSELoss(reduction="sum")
-    for _ in range(iterations):
-        outputs = model(x)
-        activations_l = activation[module_name]
-
-        l = loss(activations_l, torch.zeros_like(activations_l))
-        # l = torch.norm(torch.flatten(activations_l), 2)
-        l.retain_grad()
-        l.backward()
-
-        g = x.grad
-        if normalize:
-            g -= g.mean()
-            g /= g.std()
-
-        else:
-            g *= torch.abs(g).mean()
-
-        # print(lr * g.detach())
-        with torch.no_grad():
-            x += lr * g.detach()
-
-        model.zero_grad()
-        # x.zero_grad()
-
-    return x
 
 def show_images(img, mod_img, layer=""):
 
@@ -93,23 +90,13 @@ def show_images(img, mod_img, layer=""):
     plt.show()
 
 
+
 if __name__ == "__main__":
-    doggie: torch.Tensor = load_image("https://github.com/pytorch/hub/raw/master/images/dog.jpg")
+    deepdream = Deepdream(
+        [Deepdream.get_module_names()[5], Deepdream.get_module_names()[6]],
+        10,
+        0.000001
+    )
 
-    modules = []
-    for name, module in model.named_modules():
-        modules.append((name, module))
-
-    print(len(modules))
-
-    l = 33
-    print(f"Activation layer name: {modules[l]}")
-    mod_img = deepdream_optim(4, 0.1, doggie.clone(), modules[l][1], modules[l][1], True)[0]
-    show_images(doggie, mod_img, f"Layer number {l} of type {modules[l][1]}")
-    # for i, (name, module) in enumerate(modules[::-1]):
-    #     try:
-    #         mod_img = deepdream_optim(4, 0.01, doggie.clone(), module, name, normalize=True)[0]
-    #         show_images(doggie, mod_img, f"Layer number {i} of type {name}")
-    #     except:
-    #         continue
-    # Cool configs: (l,iter,lr)=(4,5,0.015), (5,5,0.1)
+    null_dream = deepdream.deepdream(x=torch.ones([3, 299, 299]))
+    show_images(torch.ones([3, 299, 299]), null_dream[0])
