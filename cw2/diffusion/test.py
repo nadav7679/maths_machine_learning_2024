34e0e31dd87fc8160ade78ded74d9755eec072ec
nadav7679 <nadav76799@gmail.com>
2024-03-27 11:52:09 +0000

    Refactor

--- cw2/diffusion/test.py
+++ cw2/diffusion/test.py
@@ -27,10 +27,11 @@ def plot_image_sequence(images, seq_len=10):
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 T = 1000
-diffusion = Diffusion(T, device)
+diffusion = Diffusion(T, 28, device)
 
 # Load model
-diffusion.unet = torch.load("./unet_T1000_C32_E10.tr")
+diffusion.unet = torch.load("diffusion/unet_T1000_C32_E10.tr")
+# Currently unet_T1000_C32_E10 works quite well
 
 # Sample from model
 imgs = diffusion.sample([10, 1, 28, 28])
